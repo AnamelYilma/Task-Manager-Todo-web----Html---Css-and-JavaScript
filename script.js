9a1a318282c5bd7a8ef3008c8086fbbb3334a5ea
Anamel Yilma <anamelyilma0@gmail.com>
2025-09-27 13:02:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let tasks = []
-let newtask = document.querySelector('#newTask')
+let newtask = document.querySelector('#newTask').addEventListener('click' , (e) => {
+    e.preventDefault();
+    addTask()
+})
 const addTask = () =>{
     const taskInput = document.querySelector('@taskInput')
     const textInput = taskInput.value.trim()
     if (text){
-        tasks.push({text:text , completed:false});
+        tasks.push({text:text , completed:false });
     }
-    console.log(tasks)
+    console.log(tasks);
 }
-newtask.addEventListener('click' , e => {
-    e.preventDefault();
-
-})
